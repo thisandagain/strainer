57985f9385222f8a672389c7405b07dd91ba0112
Andrew Sliwinski <thisandagain@gmail.com>
2013-04-22 19:44:29 -0700

    Normalize debt test

--- test/governance/debt.js
+++ test/governance/debt.js
@@ -2,10 +2,10 @@ var test    = require('tap').test,
     bux     = require('codebux');
 
 bux(__dirname + '/../../lib/index.js', function (err, obj) {
-    test('Debt', function (t) {
+    test('debt', function (t) {
         t.equal(err, null, 'errors should be null');
         t.type(obj, 'number', 'results should be a number');
-        t.ok(obj > 50, 'total should be greater than zero');
+        t.ok(obj > 50, 'total should be greater than debt limit');
         t.end();
     });
 });
